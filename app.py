701119959793c1248bd6298602dc780593ebf9fa
jisoo0402 <2466058@ewha.ac.kr>
2025-10-12 02:36:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
- import sys
- application = Flask(__name__)
- @application.route("/")
- def hello():
- return "Hello my webpage!"
- if __name__ == "__main__":
- application.run(host='0.0.0.0', debug=True)
+import sys
+application = Flask(__name__)
+@application.route("/")
+def hello():
+return "Hello my webpage!"
+if __name__ == "__main__":
+application.run(host='0.0.0.0', debug=True)
